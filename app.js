72f1699c7eaeddeaf86ac7f0b8424754e80d57be
Carlos Junior <carlosdossantosjuniorsp@gmail.com>
2022-02-21 21:43:41 -0300

    morgan install

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const api = express();
+const morgan = require('morgan');
 
+//Routes
 const users = require('./routes/user');
 
+api.use(morgan('dev'));
 api.use('/user', users);
 
 module.exports = api;
