ab8b1a3bf17bd8f9c145578dfcad42426d843954
Carlos Junior <carlosdossantosjuniorsp@gmail.com>
2022-02-21 22:26:21 -0300

    creating function for errors

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const api = express();
-const morgan = require('morgan');
+const morgan = require("morgan");
 
-//Routes
-const users = require('./routes/user');
+api.use(morgan("dev"));
+const users = require("./routes/user");
 
-api.use(morgan('dev'));
-api.use('/user', users);
+api.use((newError, req, res, next) => {
+    res.status(newError.status || 500);
+    return res.send({
+        error: {
+            message: newError.message,
+        },
+    });
+});
+
+api.use((error, req, next) => {
+  const reqError = new Error("Dados não encontrados!");
+  reqError.status = 404;
+  next(reqError);
+});
+
+//Routes 
+api.use("/user", users);
 
 module.exports = api;
