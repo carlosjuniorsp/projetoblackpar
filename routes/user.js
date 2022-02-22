2de205ca4bcd74a260172f4a6e9df78bebf1a2a2
Carlos Junior <carlos.junior@conam.com.br>
2022-02-22 14:49:52 -0300

    finally querys

--- routes/user.js
+++ routes/user.js
@@ -30,47 +30,88 @@ router.post("/register", (req, res, next) => {
       return res.status(500).send({ error: error });
     }
 
-  bcrypt.genSalt(saltRounds, function(err, salt) {
-    bcrypt.hash(myPlaintextPassword, salt, function(err, hash) {
-      conn.query(
-        "INSERT INTO users (name, last_name, type, phone, email, password) VALUES (?,?,?,?,?,?)",
-        [
-          req.body.name,
-          req.body.last_name,
-          req.body.type,
-          req.body.phone,
-          req.body.email,
-          hash,
-        ],
-        (error, result, field) => {
-          conn.release();
-          if (error) {
-            return res.status(500).send({ error: error, response: null });
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      bcrypt.hash(myPlaintextPassword, salt, function (err, hash) {
+        conn.query(
+          "INSERT INTO users (name, last_name, type, phone, email, password) VALUES (?,?,?,?,?,?)",
+          [
+            req.body.name,
+            req.body.last_name,
+            req.body.type,
+            req.body.phone,
+            req.body.email,
+            hash,
+          ],
+          (error, result, field) => {
+            conn.release();
+            if (error) {
+              return res.status(500).send({ error: error, response: null });
+            }
+
+            res.status(201).send({
+              msg: "Usuário cadastrado",
+              userId: result.insertId,
+            });
           }
-  
-          res.status(201).send({
-            msg: "Usuário cadastrado",
-            userId: result.insertId,
-          });
-        }
-      );
+        );
+      });
     });
-  });
-    
+
   });
 });
 
 router.delete("/delete/:id", (req, res, next) => {
-  const id = req.params.id;
-  res.status(200).send({
-    msg: "deu certo delete",
-    id: id,
+  connection.getConnection((error, conn) => {
+    if (error) {
+      return res.status(500).send({ error: error });
+    }
+    conn.query(
+      "DELETE FROM users WHERE id=?",
+      [req.params.id],
+      (error, result, field) => {
+        if (error) {
+          return res.status(500).send({ error: error });
+        }
+        return res.status(200).send({ message: 'Usuário deletado com sucesso!' });
+      }
+    );
   });
 });
 
 router.put("/update/:id", (req, res, next) => {
-  res.status(200).send({
-    msg: "deu certo put",
+  connection.getConnection((error, conn) => {
+    if (error) {
+      return res.status(500).send({ error: error });
+    }
+
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      bcrypt.hash(myPlaintextPassword, salt, function (err, hash) {
+        conn.query(
+          "UPDATE users SET name=?, last_name=?, type=?, phone=?, email=?, password=?",
+          [
+            req.body.name,
+            req.body.last_name,
+            req.body.type,
+            req.body.phone,
+            req.body.email,
+            hash,
+            req.params.id
+          ],
+          (error, result, field) => {
+            conn.release();
+            if (error) {
+              return res.status(500).send({ error: error, response: null });
+            }
+
+            res.status(201).send({
+              msg: "Usuário atualizado com sucesso!",
+              userId: result.insertId,
+            });
+          }
+        );
+      });
+    });
+
   });
 });
 
