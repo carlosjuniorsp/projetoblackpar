5e3f7061c817eae0aa6e622c675619586cb7a51f
Carlos Junior <carlosdossantosjuniorsp@gmail.com>
2022-02-23 00:36:54 -0300

    alter type the char from type

--- front/database/migrations/2014_10_12_000000_create_users_table.php
+++ front/database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('phone', 11);
-            $table->string('type', 2);
+            $table->integer('type');
         });
     }
 
