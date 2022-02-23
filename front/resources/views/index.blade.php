8765ce21894f8a436fbc15de5834b9ae0d97be81
Carlos Junior <carlosdossantosjuniorsp@gmail.com>
2022-02-23 00:04:01 -0300

    creating login the user

---
+++ front/resources/views/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Projeto</title>
+</head>
+
+<body>
+    <div class="container-sm col-md-4">
+        <h3>Projeto</h3>
+        <ul class="nav nav-tabs">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Cadastro</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Editar</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Buscar Vídeos</a>
+            </li>
+        </ul>
+    </div>
+</body>
+
+</html>
