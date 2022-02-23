c78c729044243be891be7320e2f861a13b4ee750
Carlos Junior <carlosdossantosjuniorsp@gmail.com>
2022-02-22 22:14:47 -0300

    guzzle install

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const connection = require("../connection").pool;
+const config = require("../config");
 
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 router.post("/", (req, res, next) => {
   connection.getConnection((error, conn) => {
@@ -10,7 +12,7 @@ router.post("/", (req, res, next) => {
       return res.status(500).send({ error: error });
     }
     conn.query(
-      "SELECT email, password FROM users WHERE email = ?",
+      "SELECT email, password,id, name, type FROM users WHERE email = ?",
       [req.body.email],
       (error, results, field) => {
         conn.release();
@@ -20,15 +22,32 @@ router.post("/", (req, res, next) => {
         if (results.length < 1) {
           return res.status(401).send({ msg: "Falha na autenticação" });
         }
-        bcrypt.compare(req.body.password, results[0].password, (err, result) => {
-          if (err) {
+        bcrypt.compare(
+          req.body.password,
+          results[0].password,
+          (err, result) => {
+            if (err) {
+              return res.status(401).send({ msg: "Falha na autenticação" });
+            }
+            if (result) {
+              const token = jwt.sign(
+                {
+                  id: results[0].id,
+                  name: results[0].name,
+                  email: results[0].email,
+                },
+                config.JWT_USER_TOKEN,
+                {
+                  expiresIn: "5h",
+                }
+              );
+              return res
+                .status(200)
+                .send({ msg: "Login efetuado", token: token });
+            }
             return res.status(401).send({ msg: "Falha na autenticação" });
           }
-          if (result) {
-            return res.status(200).send({ msg: "Autenticado" });
-          }
-          return res.status(401).send({ msg: "Falha na autenticação" });
-        });
+        );
       }
     );
   });
