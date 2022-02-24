a6fa3273d69937da98f2d3f8022b1076c58a4c99
Carlos Junior <carlos.junior@conam.com.br>
2022-02-24 18:16:24 -0300

    validate history

--- front/resources/views/history.blade.php
+++ front/resources/views/history.blade.php
@@ -5,41 +5,50 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="card">
-                        <div class="card-body">
-                            @if (Session::has('type') == 0)
-                                )
-                                <h5 class="card-title text-uppercase mb-0">Histórico de busca do usuário</h5>
+                        @if (empty($data))
+                            <div class="card-body">
+
+                                <h5 class="card-title text-uppercase mb-0">Sem histórico para exibir</h5>
                             @else
-                                <h5 class="card-title text-uppercase mb-0">Meu histórico de busca</h5>
-                            @endif
-                        </div>
-                        <div class="table-responsive">
-                            <table class="table no-wrap user-table mb-0">
-                                <thead>
-                                    <tr>
-                                        <th scope="col" class="border-0 text-uppercase font-medium pl-4">#</th>
-                                        <th scope="col" class="border-0 text-uppercase font-medium">Titulo do vídeo</th>
-                                        <th scope="col" class="border-0 text-uppercase font-medium">Data</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($data ?? '' as $history)
+                                @if (Session::has('type') == 0)
+                                    )
+                                    <h5 class="card-title text-uppercase mb-0">Histórico de busca do usuário</h5>
+                                @else
+                                    <h5 class="card-title text-uppercase mb-0">Meu histórico de busca</h5>
+                                @endif
+
+                            </div>
+
+
+                            <div class="table-responsive">
+                                <table class="table no-wrap user-table mb-0">
+                                    <thead>
                                         <tr>
-                                            <td class="pl-4">{{ $history['id'] }}</td>
-                                            <td>
-                                                <h5 class="font-medium mb-0"> {{ $history['title'] }}</h5>
-                                            </td>
-                                            <td>
-                                                <span
-                                                    class="text-muted">{{ date('d/m/Y', strtotime($history['date'])) }}
-                                                </span><br>
-                                            </td>
+                                            <th scope="col" class="border-0 text-uppercase font-medium pl-4">#</th>
+                                            <th scope="col" class="border-0 text-uppercase font-medium">Titulo do vídeo
+                                            </th>
+                                            <th scope="col" class="border-0 text-uppercase font-medium">Data</th>
                                         </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($data ?? '' as $history)
+                                            <tr>
+                                                <td class="pl-4">{{ $history['id'] }}</td>
+                                                <td>
+                                                    <h5 class="font-medium mb-0"> {{ $history['title'] }}</h5>
+                                                </td>
+                                                <td>
+                                                    <span
+                                                        class="text-muted">{{ date('d/m/Y', strtotime($history['date'])) }}
+                                                    </span><br>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
                     </div>
+                    @endif
                 </div>
             </div>
         </div>
